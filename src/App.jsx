e44aa90f147806297fa4a68117ee7c747b9b31ab
oualidAtiya <atiyaoualid@gmail.com>
2025-04-02 15:51:32 +0000

    Add Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cards from "./components/Cards";
 import Header from "./components/Header";
 import Slider from "./components/Slider";
 
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <Header/>
       <Slider/>
+      <Cards/>
     </>
   )
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+export default function Cards() {
+    return(
+        <div className="flex items-center justify-between p-[30px] h-[250px] ">
+            <div className="bg-[url(/images/card1.jpg)] w-1/3 h-full bg-[-75px_0px] bg-cover bg-no-repeat  pt-[40px] pl-[140px]">
+                <h3 className="mb-[10px] font-light">2025 FaShion</h3>
+                <h2 className="mb-[20px] font-font-semibold" >Locomotive Men White T-shirt</h2>
+                <button className="underline cursor-pointer">Shop Now</button>
+            </div>
+            <div className="bg-[url(/images/card1.jpg)] w-1/3 h-full bg-[-75px_0px] bg-cover bg-no-repeat  pt-[40px] pl-[140px]">
+                <h3 className="mb-[10px] font-light">2025 FaShion</h3>
+                <h2 className="mb-[20px] font-font-semibold" >Locomotive Men White T-shirt</h2>
+                <button className="underline cursor-pointer">Shop Now</button>
+            </div>
+            <div className="bg-[url(/images/card3.jpg)] w-1/3 h-full bg-[-75px_0px] bg-cover bg-no-repeat  pt-[40px] pl-[140px]">
+                <h3 className="mb-[10px] font-light">2025 FaShion</h3>
+                <h2 className="mb-[20px] font-font-semibold" >Locomotive Men White T-shirt</h2>
+                <button className="underline cursor-pointer">Shop Now</button>
+            </div>
+        </div>
+    )
+}
