cbe67d9737f56885d1c83d4416fa92e25499ef83
oualidAtiya <atiyaoualid@gmail.com>
2025-04-04 16:51:26 +0000

    Finishing Latest Products Part

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import LatestProducts from "./components/LatestProducts";
 import Slider from "./components/Slider";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Header/>
       <Slider/>
       <Cards/>
+      <LatestProducts/>
     </>
   )
 }
---
+++ src/components/LatestProducts.jsx
@@ -0,0 +1,81 @@
+export default  function LatestProducts(){
+    return(
+            <div class="bg-white">
+                <div class="mx-auto max-w-2xl px-4 py-16 sm:px-2 sm:py-[24px] lg:max-w-7xl lg:px-[30px]">
+                    <h2 class="text-2xl font-bold tracking-tight text-slate-900">Latest Products</h2>
+
+                    <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    <div class="group relative">
+                        <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+                        <div class="mt-4 flex justify-between">
+                        <div>
+                                <h3 class="text-[17px] text-slate-900">
+                                Men Full Sleeve Color Block Sweatshirt Latest SkyBlue
+                                </h3>
+                            </div>
+                        </div>
+                            <p class="text-[13px] font-medium text-yellow-500">
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star-half-stroke"></i>
+                            </p>
+                            <p class="text-lg font-medium text-slate-900">$35</p>
+                    </div>
+                    <div class="group relative">
+                        <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+                        <div class="mt-4 flex justify-between">
+                        <div>
+                                <h3 class="text-[17px] text-slate-900">
+                                Men Full Sleeve Color Block Sweatshirt Latest SkyBlue
+                                </h3>
+                            </div>
+                        </div>
+                            <p class="text-[13px] font-medium text-yellow-500">
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star-half-stroke"></i>
+                            </p>
+                            <p class="text-lg font-medium text-slate-900">$35</p>
+                    </div>
+                    <div class="group relative">
+                        <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+                        <div class="mt-4 flex justify-between">
+                        <div>
+                                <h3 class="text-[17px] text-slate-900">
+                                Men Full Sleeve Color Block Sweatshirt Latest SkyBlue
+                                </h3>
+                            </div>
+                        </div>
+                            <p class="text-[13px] font-medium text-yellow-500">
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star-half-stroke"></i>
+                            </p>
+                            <p class="text-lg font-medium text-slate-900">$35</p>
+                    </div>
+                    <div class="group relative">
+                        <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+                        <div class="mt-4 flex justify-between">
+                        <div>
+                                <h3 class="text-[17px] text-slate-900">
+                                Men Full Sleeve Color Block Sweatshirt Latest SkyBlue
+                                </h3>
+                            </div>
+                        </div>
+                            <p class="text-[13px] font-medium text-yellow-500">
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star mr-[5px]"></i>
+                                <i class="fa-solid fa-star-half-stroke"></i>
+                            </p>
+                            <p class="text-lg font-medium text-slate-900">$35</p>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+    )
+}
